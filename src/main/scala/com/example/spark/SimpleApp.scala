ec87a2fba448825e4e8af3fd4c9bb13069171d71
Your Name <you@example.com>
2017-02-07 21:41:14 +0900

    up

--- src/main/scala/com/example/spark/SimpleApp.scala
+++ src/main/scala/com/example/spark/SimpleApp.scala
@@ -6,20 +6,29 @@ import org.apache.spark.SparkConf
 import org.apache.spark.sql.SQLContext
 import scala.util.Try
 import scala.util.control.Exception._
-
+/*
+date_time,ip,request_uri,ipao97_value,referer,useragent,tuuid,account_id,data_owner_id,os,os_version,browser,browser_version,url_category_ids,event_ids,keyword_group_ids,keywords,segment_ids,gender_age,income,marriage,occupation,frequency_of_ec_buying,amount_of_ec_buying,brand_vs_price_oriented,children_adult,children_university,children_high_school,children_middle_school,children_elementary_school,children_preschooler,work_location,home_location,work_location_zipcode,home_location_zipcode,carrier,time
+*/
 class CSVParser(header:Array[String]) extends Serializable {
   val index = header.zipWithIndex.toMap
+  def convert(keys:Array[String], array:Array[String]) = Try { 
+     keys map { x =>
+       array(index(x))
+     } toList
+  }
   def convert(key:String, array:Array[String]) = Try { 
      array(index(key))
+  }
+  def apply(keys:Array[String], array:Array[String]):List[String] = {
+     convert(keys, array).getOrElse( List.fill(keys.length)("___PARSE_ERR___") )
   }
   def apply(key:String, array:Array[String]):String = {
      convert(key, array).getOrElse("___PARSE_ERR___")
   }
 }
 object SimpleCSV {
-  def main(args: Array[String]) {
-    val logFile = "/mnt/sdb1/hadoop/201702_huy_1.5m.csv"
-    val conf    = new SparkConf().setAppName("Simple Parser")
+  def getData(logFile:String) = {
+    val conf    = new SparkConf().setAppName("Simple Parser").set("spark.driver.allowMultipleContexts", "true")
     val sc      = new SparkContext(conf)
     val csv     = sc.textFile(logFile)
     val data    = csv map { x => 
@@ -44,15 +53,33 @@ object SimpleCSV {
        } 
     } 
     val parse  = new CSVParser(data.take(1)(0)) 
-    val rows = data filter { x => 
+    val rows   = data filter { x => 
       parse("tuuid", x) != "tuuid"
     }
+    (parse, rows)
+  }
+  def main(args: Array[String]) {
+    val pr = getData("/mnt/sdb1/hadoop/201702_huy_0.5m.csv")
+    val pr2 = getData("/mnt/sdb1/hadoop/201702_huy_1.5m.csv")
+    val parse = pr._1
+    val rows  = pr._2
+    //val rows2 = pr2._2
     val tuuids = rows map { x => 
       parse("tuuid", x) 
-    } 
-
-    tuuids take(100000) map { x => 
+    }
+    tuuids take(100) map { x => 
       println(x)
     }
+    /*
+    val tuuid_ip_requesturi = rows map { x => 
+      parse(Array("tuuid", "ip", "request_uri"), x)
+    } filter { x => 
+      //x(0) == "10582c4c-3672-4996-9815-bc1b0a420f92"
+      x(2).matches("""cosmowater""")
+    }
+
+    tuuid_ip_requesturi take(10) map { x => 
+      println(x)
+    }*/
   }
 }
